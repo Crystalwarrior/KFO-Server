d175642145dd3387cdea85937bd2bfe5cc3cad5d
Chrezm <TheChrezm@gmail.com>
2019-10-18 20:16:57 -0400

    Forgot to update revision number

--- server/tsuserver.py
+++ server/tsuserver.py
@@ -43,8 +43,8 @@ class TsuserverDR:
         self.release = 4
         self.major_version = 2
         self.minor_version = 0
-        self.segment_version = 'a23'
-        self.internal_version = '191018b'
+        self.segment_version = 'a24'
+        self.internal_version = '191018c'
         version_string = self.get_version_string()
         self.software = 'TsuserverDR {}'.format(version_string)
         self.version = 'TsuserverDR {} ({})'.format(version_string, self.internal_version)
