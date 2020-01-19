037623ff69e0ec1fa658735f897e12c0e6e48d6e
Chrezm <TheChrezm@gmail.com>
2020-01-19 11:29:17 -0500

    Fix extra text for /look_set+'statue'->'statue' in /passage_restore

--- server/tsuserver.py
+++ server/tsuserver.py
@@ -42,8 +42,8 @@ class TsuserverDR:
         self.release = 4
         self.major_version = 3
         self.minor_version = 0
-        self.segment_version = 'a4'
-        self.internal_version = '200115b'
+        self.segment_version = 'a6'
+        self.internal_version = '200119b'
         version_string = self.get_version_string()
         self.software = 'TsuserverDR {}'.format(version_string)
         self.version = 'TsuserverDR {} ({})'.format(version_string, self.internal_version)
