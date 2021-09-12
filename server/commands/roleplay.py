c5e5fbd3f36172a0c968be1ee9fd9da196ddbe33
Rosemary Witchaven <32779090+in1tiate@users.noreply.github.com>
2021-09-12 01:47:30 -0500

    fix small blunder in rolldice

--- server/commands/roleplay.py
+++ server/commands/roleplay.py
@@ -163,7 +163,7 @@ def ooc_cmd_rollp(client, arg):
     """
     roll, num_dice, chosen_max, modifiers, Sum = rtd(arg)
 
-    client.send_ooc(f'[Hidden] You rolled {roll} out of {chosen_max}.\nThe total sum is {Sum}.' + f'\nThe total sum is {Sum}.' if num_dice > 1 else '')
+    client.send_ooc(f'[Hidden] You rolled {roll} out of {chosen_max}.' + f'\nThe total sum is {Sum}.' if num_dice > 1 else '')
     for c in client.area.owners:
         c.send_ooc(f'[{client.area.id}]{client.showname} secretly rolled {roll} out of {chosen_max}.')
 
