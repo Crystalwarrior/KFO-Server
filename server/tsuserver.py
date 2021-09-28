736c9c7754da361af1e62a02f84c53397004c6e8
Chrezm <TheChrezm@gmail.com>
2021-09-28 08:13:28 -0400

    Readying up for release

--- server/tsuserver.py
+++ server/tsuserver.py
@@ -68,8 +68,8 @@ class TsuserverDR:
         self.release = 4
         self.major_version = 3
         self.minor_version = 0
-        self.segment_version = 'b202'
-        self.internal_version = 'M210919a'
+        self.segment_version = 'RC1'
+        self.internal_version = 'M210928a'  # Remove M for release
         version_string = self.get_version_string()
         self.software = 'TsuserverDR {}'.format(version_string)
         self.version = 'TsuserverDR {} ({})'.format(version_string, self.internal_version)
