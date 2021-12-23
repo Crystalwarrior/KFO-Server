191e27e755a19a344bb21da1e8c4c8df2c715735
EstatoDeviato <90421310+EstatoDeviato@users.noreply.github.com>
2021-12-23 14:14:32 +0100

    Delete Start - Linux.sh

--- Start - Linux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-while [ true ]
-do
-	echo "Starting up server..."
-	python3.8 start_server.py
-	echo "Server has shut down. Will restart in 2 seconds (use CTRL-C to cancel)"
-	sleep 2
-done
-
