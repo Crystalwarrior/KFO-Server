30c9a4f6d90ec3b8ad8aad6dbf04ff7268744107
Alex Noir <Varsash@gmail.com>
2022-02-11 22:28:09 +0300

    fix y_offset missing from TestClientConnection checker

--- tests/test_client_connection.py
+++ tests/test_client_connection.py
@@ -2,7 +2,7 @@ from .structures import _Unittest
 
 _standard_FL = ('yellowtext', 'customobjections', 'flipping', 'fastloading', 'noencryption',
                 'deskmod', 'evidence', 'cccc_ic_support', 'looping_sfx', 'additive', 'effects',
-                'ackMS', 'showname', 'chrini', 'charscheck')
+                'y_offset', 'ackMS', 'showname', 'chrini', 'charscheck')
 
 
 class TestClientConnection(_Unittest):
