7ff1f6bcb868d6008c397471c7616f54fbeb0eab
Satoru;1816 <81326989+Satoru-1816@users.noreply.github.com>
2022-02-11 19:33:32 -0300

    Update client_manager.py

--- server/client_manager.py
+++ server/client_manager.py
@@ -865,10 +865,8 @@ class ClientManager:
                 if not old_area.dark and not old_area.force_sneak:
                     if old_area.area_manager == self.area.area_manager:
                         if self.area.area_manager.passing_msg == True:
-                            if showname == '':
-                                showname = self.char_name
                             old_area.send_ic(
-                                None, '1', 1, "", "", f'~~[º{self.showname}º leaves to º{area.name}º.]', 
+                                None, '1', 1, "", "", f'~~}}[º{self.showname}º leaves to º{area.name}º.]', 
                                 "", "", 1, -1, 0, 0, [0], 0, 0, 0, "", -1, "", "", 0, 0, 0, 0, "0", 0, "", "", "", 0, ""
                             )
                         for c in old_area.clients:
@@ -913,10 +911,8 @@ class ClientManager:
                         "1",
                     )
                     if self.area.area_manager.passing_msg == True:
-                        if showname == '':
-                            showname = self.char_name
                         self.area.send_ic(
-                            None, '1', 1, "", "", f'~~[º{self.showname}º enters from º{old_area.name}º.]', 
+                            None, '1', 1, "", "", f'~~}}[º{self.showname}º enters from º{old_area.name}º.]', 
                             "", "", 1, -1, 0, 0, [0], 0, 0, 0, "", -1, "", "", 0, 0, 0, 0, "0", 0, "", "", "", 0, ""
                         )
                 else:
