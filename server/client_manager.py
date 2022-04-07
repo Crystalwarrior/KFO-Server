b79aacb38535799f7c9be91aba8f6fade3cce2c3
EstatoDeviato <90421310+EstatoDeviato@users.noreply.github.com>
2022-04-07 12:22:51 +0200

    Fix masterserver_name is None

--- server/client_manager.py
+++ server/client_manager.py
@@ -644,7 +644,9 @@ class ClientManager:
                     c.send_command(
                         "FA",
                         *[
-                            f"{self.server.config['masterserver_name']}\n Users Online: {server_count}\n Double-Click me to see Areas\n _______",
+                            f"{self.server.config['masterserver_name']}\n Users Online: {server_count}\n Double-Click me to see Areas\n _______" 
+                            if self.server.config['masterserver_name'] is not None
+                            else "New Server\n Users Online: {server_count}\n Double-Click me to see Areas\n _______",
                             *[
                                 f"[{hub.id}] {hub.name} (users: {hub.count})"
                                 for hub in self.server.hub_manager.hubs
@@ -1537,7 +1539,9 @@ class ClientManager:
                 c.send_command(
                     "FA",
                     *[
-                        f"{self.server.config['masterserver_name']}\n Users Online: {server_count}\n Double-Click me to see Areas\n _______",
+                        f"{self.server.config['masterserver_name']}\n Users Online: {server_count}\n Double-Click me to see Areas\n _______"
+                        if self.server.config['masterserver_name'] is not None
+                        else f"New Server\n Users Online: {server_count}\n Double-Click me to see Areas\n _______",
                         *[
                             f"[{hub.id}] {hub.name} (users: {hub.count})"
                             for hub in self.server.hub_manager.hubs
