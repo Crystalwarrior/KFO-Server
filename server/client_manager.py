9da12945acfabc3cd2d179f8113416e685fa1db0
Chrezm <TheChrezm@gmail.com>
2022-04-26 12:26:42 -0400

    Trim outgoing file extension for non-DRO 1.1.0

--- server/client_manager.py
+++ server/client_manager.py
@@ -632,8 +632,10 @@ class ClientManager:
 
         def send_music(self, name=None, char_id=None, showname=None, force_same_restart=None,
                        loop=None, channel=None, effects=None):
-            if self.packet_handler == clients.ClientDRO1d1d0:
-                name = name.replace('.mp3', '')
+            if self.packet_handler != clients.ClientDRO1d1d0:
+                file_extension_location = name.rfind('.')
+                if file_extension_location >= 0:
+                    name = name[:file_extension_location]
 
             self.send_command_dict('MC', {
                 'name': name,
