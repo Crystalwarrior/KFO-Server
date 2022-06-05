ffa53739f47b1855f76aa0d91770894374884fed
Dev <90421310+EstatoDeviato@users.noreply.github.com>
2022-06-05 06:09:51 +0200

    Update tsuserver.py

--- server/tsuserver.py
+++ server/tsuserver.py
@@ -485,7 +485,7 @@ class TsuServer3:
 
     def send_discord_chat(self, name, message, hub_id=0, area_id=0):
         area = self.hub_manager.get_hub_by_id(hub_id).get_area_by_id(area_id)
-        cid = self.get_char_id_by_name(self.config["bridgebot"]["character"])
+        cid = area.area_manager.get_char_id_by_name(self.config["bridgebot"]["character"])
         message = dezalgo(message)
         message = remove_URL(message)
         message = (
