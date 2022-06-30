0da1dea383ffecfa837f36fef36b67a6ebffd9c9
Alex Noir <Varsash@gmail.com>
2022-06-30 23:45:06 +0300

    update the bash script to be "python3" rather than "python3.8"

--- Start - Linux.sh
+++ Start - Linux.sh
@@ -2,7 +2,7 @@
 while [ true ]
 do
 	echo "Starting up server..."
-	python3.8 start_server.py
+	python3 start_server.py
 	echo "Server has shut down. Will restart in 2 seconds (use CTRL-C to cancel)"
 	sleep 2
 done
