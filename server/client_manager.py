0a21dbd1f8a9f32aa5f29dcb541ac3c9afc4db9a
Alex Noir <Varsash@gmail.com>
2024-03-05 01:18:01 +0300

    Fix area.area_manager.hub reference (area_manager *is* the hub)

--- server/client_manager.py
+++ server/client_manager.py
@@ -1333,7 +1333,6 @@ class ClientManager:
             pathlocked = (
                 "🚧"
                 if str(area.id) in self.area.links
-                and area.area_manager.hub == self.area.area_manager.hub
                 and self.area.links[str(area.id)]["locked"]
                 else ""
             )
