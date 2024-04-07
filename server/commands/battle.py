23cbeba3cf62b3e18eb8b6017df1134a895bd4cb
Dev <90421310+EstatoDeviato@users.noreply.github.com>
2024-04-07 17:21:46 +0200

    Update battle.py

--- server/commands/battle.py
+++ server/commands/battle.py
@@ -1,5 +1,6 @@
 import yaml
 import random
+import os
 
 from server.client_manager import ClientManager
 
@@ -57,13 +58,15 @@ def ooc_cmd_choose_fighter(client, arg):
     You will receive its stats and its moves.
     Usage: /choose_fighter NameFighter
     """
-    with open("storage/battlesystem/battlesystem.yaml", "r", encoding="utf-8") as c:
-        char = yaml.safe_load(c)
-        if arg.lower() in char:
-            client.battle = ClientManager.Battle_char(client, arg.lower())
-            send_info_fighter(client)
-        else:
-            client.send_ooc("No fighter has this name!")
+    if f"{arg.lower()}.yaml" in os.listdir("storage/battlesystem"):
+        with open(
+            f"storage/battlesystem/{arg.lower()}.yaml", "r", encoding="utf-8"
+        ) as c_load:
+            char = yaml.safe_load(c_load)
+            client.battle = ClientManager.BattleChar(client, arg.lower(), char)
+        send_info_fighter(client)
+    else:
+        client.send_ooc("No fighter has this name!")
 
 
 def ooc_cmd_info_fighter(client, arg):
@@ -83,57 +86,48 @@ def ooc_cmd_create_fighter(client, arg):
     Usage: /create_fighter FighterName HP ATK DEF SPA SPD SPE
     """
     args = arg.split(" ")
-    if len(args) < 8:
-        if len(args) > 6:
-            if (
-                args[1].isdigit()
-                and args[2].isdigit()
-                and args[3].isdigit()
-                and args[4].isdigit()
-                and args[5].isdigit()
-                and args[6].isdigit()
-            ):
-                if (
-                    float(args[1]) > 0
-                    and float(args[2]) > 0
-                    and float(args[3]) > 0
-                    and float(args[4]) > 0
-                    and float(args[5]) > 0
-                    and float(args[6]) > 0
-                ):
-                    with open(
-                        "storage/battlesystem/battlesystem.yaml", "r", encoding="utf-8"
-                    ) as c_load:
-                        char = yaml.safe_load(c_load)
-                        args[0] = args[0].lower()
-                        if args[0] not in char:
-                            char[args[0]] = {}
-                            char[args[0]]["HP"] = float(args[1])
-                            char[args[0]]["ATK"] = float(args[2])
-                            char[args[0]]["DEF"] = float(args[3])
-                            char[args[0]]["SPA"] = float(args[4])
-                            char[args[0]]["SPD"] = float(args[5])
-                            char[args[0]]["SPE"] = float(args[6])
-                            char[args[0]]["Moves"] = []
-                            with open(
-                                "storage/battlesystem/battlesystem.yaml",
-                                "w",
-                                encoding="utf-8",
-                            ) as c_save:
-                                yaml.dump(char, c_save)
-                                client.send_ooc(f"{args[0]} has been created!")
-                        else:
-                            client.send_ooc("This fighter has already been created.")
-                else:
-                    client.send_ooc(
-                        "hp, atk, def, spa, spd, spe have to be greater than zero!"
-                    )
-            else:
-                client.send_ooc("Some argument is not an integer.")
-        else:
-            client.send_ooc("Not enough arguments...")
-    else:
+
+    if len(args) > 7:
         client.send_ooc("Too many arguments...")
+        return
+
+    if len(args) < 7:
+        client.send_ooc("Not enough arguments...")
+        return
+
+    if (
+        float(args[1]) <= 0
+        or float(args[2]) <= 0
+        or float(args[3]) <= 0
+        or float(args[4]) <= 0
+        or float(args[5]) <= 0
+        or float(args[6]) <= 0
+    ):
+        client.send_ooc("hp, atk, def, spa, spd, spe have to be greater than zero!")
+        return
+
+    fighter_list = os.listdir("storage/battlesystem")
+
+    if f"{args[0].lower()}.yaml" in fighter_list:
+        client.send_ooc("This fighter has already been created.")
+        return
+
+    fighter = {}
+    fighter["HP"] = float(args[1])
+    fighter["ATK"] = float(args[2])
+    fighter["DEF"] = float(args[3])
+    fighter["SPA"] = float(args[4])
+    fighter["SPD"] = float(args[5])
+    fighter["SPE"] = float(args[6])
+    fighter["Moves"] = []
+
+    with open(
+        f"storage/battlesystem/{args[0].lower()}.yaml",
+        "w",
+        encoding="utf-8",
+    ) as c_save:
+        yaml.dump(fighter, c_save)
+        client.send_ooc(f"{args[0]} has been created!")
 
 
 def ooc_cmd_create_move(client, arg):
@@ -143,54 +137,58 @@ def ooc_cmd_create_move(client, arg):
     MovesType: Atk or Spa
     Usage: /create_move MoveName MovesType Power Accuracy Effects
     """
-    if client.battle is not None:
-        args = arg.split(" ")
-        if args[2].isdigit and float(args[2]) > 0:
-            if args[3].isdigit and float(args[3]) > 0 and float(args[3]) < 101:
-                if args[1].lower() in ["atk", "spa"]:
-                    with open(
-                        "storage/battlesystem/battlesystem.yaml", "r", encoding="utf-8"
-                    ) as c_load:
-                        char = yaml.safe_load(c_load)
-                        fighter = client.battle.fighter
-                        move_list = []
-                        for i in range(0, len(char[fighter]["Moves"])):
-                            move_list.append(char[fighter]["Moves"][i]["Name"])
-                        if args[0].lower() not in move_list:
-                            char[fighter]["Moves"].append({})
-                            index = len(char[fighter]["Moves"]) - 1
-                            char[fighter]["Moves"][index]["Name"] = args[0].lower()
-                            char[fighter]["Moves"][index]["MovesType"] = args[1].lower()
-                            char[fighter]["Moves"][index]["Power"] = float(args[2])
-                            char[fighter]["Moves"][index]["Accuracy"] = float(args[3])
-                            char[fighter]["Moves"][index]["Effects"] = []
-                            for i in range(4, len(args)):
-                                if args[i].lower() in battle_effects:
-                                    char[fighter]["Moves"][index]["Effects"].append(
-                                        args[i].lower()
-                                    )
-                            with open(
-                                "storage/battlesystem/battlesystem.yaml",
-                                "w",
-                                encoding="utf-8",
-                            ) as c_save:
-                                yaml.dump(char, c_save)
-                                client.battle = ClientManager.Battle_char(
-                                    client, fighter
-                                )
-                                client.send_ooc(f"{args[0]} has been added!")
-                        else:
-                            client.send_ooc("This move has already been created.")
-                else:
-                    client.send_ooc("Move's Type should be atk or spa!")
-            else:
-                client.send_ooc("Accuracy should be a integer between 1 and 100")
-        else:
-            client.send_ooc("Power argument is not an integer.")
-    else:
+    if client.battle is None:
         client.send_ooc(
             "You have to choose a figher to create a move.\n /choose_fighter FighterName"
         )
+        return
+
+    args = arg.split(" ")
+
+    if float(args[2]) <= 0:
+        client.send_ooc("Power has to be greater than 0.")
+        return
+
+    if float(args[3]) <= 0 or float(args[3]) > 100:
+        client.send_ooc("Accuracy should be a integer between 1 and 100")
+        return
+
+    if args[1].lower() not in ["atk", "spa"]:
+        client.send_ooc("Move's Type should be atk or spa!")
+        return
+
+    with open(
+        f"storage/battlesystem/{client.battle.fighter}.yaml", "r", encoding="utf-8"
+    ) as c_load:
+        char = yaml.safe_load(c_load)
+
+        move_list = []
+        for i in range(0, len(char["Moves"])):
+            move_list.append(char["Moves"][i]["Name"])
+
+        if args[0].lower() in move_list:
+            client.send_ooc("This move has already been created.")
+            return
+
+        char["Moves"].append({})
+        index = len(char["Moves"]) - 1
+        char["Moves"][index]["Name"] = args[0].lower()
+        char["Moves"][index]["MovesType"] = args[1].lower()
+        char["Moves"][index]["Power"] = float(args[2])
+        char["Moves"][index]["Accuracy"] = float(args[3])
+        char["Moves"][index]["Effects"] = []
+        for i in range(4, len(args)):
+            if args[i].lower() in battle_effects:
+                char["Moves"][index]["Effects"].append(args[i].lower())
+        with open(
+            f"storage/battlesystem/{client.battle.fighter}.yaml",
+            "w",
+            encoding="utf-8",
+        ) as c_save:
+            yaml.dump(char, c_save)
+
+        client.send_ooc(f"{args[0]} has been added!")
+        client.battle = ClientManager.BattleChar(client, client.battle.fighter, char)
 
 
 @mod_only(hub_owners=True)
@@ -200,29 +198,32 @@ def ooc_cmd_modify_stat(client, arg):
     Usage: /modify_stat FighterName Stat Value
     """
     args = arg.split(" ")
+    if f"{args[0].lower()}.yaml" not in os.listdir("storage/battlesystem"):
+        client.send_ooc("No fighter has this name!")
+        return
+
+    if args[1].lower() not in ["hp", "atk", "def", "spa", "spd", "spe"]:
+        client.send_ooc(
+            "You could just modify this stats:\nhp, atk, def, spa, spd, spe"
+        )
+        return
+
+    if float(args[2]) <= 0:
+        client.send_ooc("The value have to be a number greater than zero")
+        return
+
     with open(
-        "storage/battlesystem/battlesystem.yaml", "r", encoding="utf-8"
+        f"storage/battlesystem/{args[0].lower()}.yaml", "r", encoding="utf-8"
     ) as c_load:
         char = yaml.safe_load(c_load)
-        if args[0].lower() in char:
-            if args[1].lower() in ["hp", "atk", "def", "spa", "spd", "spe"]:
-                if args[2].isdigit and float(args[2]) > 0:
-                    char[args[0].lower()][args[1].upper()] = float(args[2])
-                    with open(
-                        "storage/battlesystem/battlesystem.yaml", "w", encoding="utf-8"
-                    ) as c_save:
-                        yaml.dump(char, c_save)
-                        client.send_ooc(
-                            f"{args[0]}'s {args[1]} has been modified. To check the changes choose again this fighter"
-                        )
-                else:
-                    client.send_ooc("The value have to be an integer greater than zero")
-            else:
-                client.send_ooc(
-                    "You could just modify this stats:\nhp, atk, def, spa, spd, spe"
-                )
-        else:
-            client.send_ooc("No fighter has this name!")
+        char[args[1].upper()] = float(args[2])
+        with open(
+            f"storage/battlesystem/{args[0].lower()}.yaml", "w", encoding="utf-8"
+        ) as c_save:
+            yaml.dump(char, c_save)
+    client.send_ooc(
+        f"{args[0]}'s {args[1]} has been modified. To check the changes choose again this fighter"
+    )
 
 
 @mod_only(hub_owners=True)
@@ -231,19 +232,11 @@ def ooc_cmd_delete_fighter(client, arg):
     Allow you to delete a fighter.
     Usage: /delete_move FighterName
     """
-    with open(
-        "storage/battlesystem/battlesystem.yaml", "r", encoding="utf-8"
-    ) as c_load:
-        char = yaml.safe_load(c_load)
-        if arg.lower() in char:
-            char.pop(arg.lower())
-            with open(
-                "storage/battlesystem/battlesystem.yaml", "w", encoding="utf-8"
-            ) as c_save:
-                yaml.dump(char, c_save)
-                client.send_ooc(f"{arg} has been deleted!")
-        else:
-            client.send_ooc(f"{arg} is not found in the fighter server list.")
+    if f"{arg.lower()}.yaml" in os.listdir("storage/battlesystem"):
+        os.remove(f"storage/battlesystem/{arg.lower()}.yaml")
+        client.send_ooc(f"{arg} has been deleted!")
+    else:
+        client.send_ooc(f"{arg} is not found in the fighter server list.")
 
 
 @mod_only(hub_owners=True)
@@ -253,29 +246,34 @@ def ooc_cmd_delete_move(client, arg):
     You have to choose a fighter first!
     Usage: /delete_move MoveName
     """
-    if client.battle is not None:
-        with open(
-            "storage/battlesystem/battlesystem.yaml", "r", encoding="utf-8"
-        ) as c_load:
-            char = yaml.safe_load(c_load)
-            move_list = []
-            for i in range(0, len(char[client.battle.fighter]["Moves"])):
-                move_list.append(char[client.battle.fighter]["Moves"][i]["Name"])
-            if arg.lower() in move_list:
-                index = move_list.index(arg.lower())
-                char[client.battle.fighter]["Moves"].pop(index)
-                with open(
-                    "storage/battlesystem/battlesystem.yaml", "w", encoding="utf-8"
-                ) as c_save:
-                    yaml.dump(char, c_save)
-                    client.battle = ClientManager.Battle_char(
-                        client, client.battle.fighter
-                    )
-                    client.send_ooc(f"{arg} has been deleted!")
-            else:
-                client.send_ooc(f"{arg} is not found in the fighter moves")
-    else:
+    
+    if client.battle is None:
         client.send_ooc("You have to choose the fighter first")
+        return
+    
+    with open(
+        f"storage/battlesystem/{client.battle.fighter}.yaml", "r", encoding="utf-8"
+    ) as c_load:
+        char = yaml.safe_load(c_load)
+        move_list = []
+        for i in range(0, len(char["Moves"])):
+            move_list.append(char["Moves"][i]["Name"])
+        if arg.lower() in move_list:
+            index = move_list.index(arg.lower())
+            char["Moves"].pop(index)
+            with open(
+                f"storage/battlesystem/{client.battle.fighter}.yaml",
+                "w",
+                encoding="utf-8",
+            ) as c_save:
+                yaml.dump(char, c_save)
+
+            client.battle = ClientManager.BattleChar(
+                client, client.battle.fighter, char
+            )
+            client.send_ooc(f"{arg} has been deleted!")
+        else:
+            client.send_ooc(f"{arg} is not found in the fighter moves")
 
 
 @mod_only(hub_owners=True)
