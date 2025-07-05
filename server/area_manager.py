a2ac11f752b9defe888eeb074f9f07445b7fee4e
Alex Noir <varsash@gmail.com>
2025-07-06 00:27:44 +0300

    Fix bug where charlist_ref was not being used, resulting in character list updating over and over again even when it shouldn't

--- server/area_manager.py
+++ server/area_manager.py
@@ -251,18 +251,18 @@ class AreaManager:
         """Load the character list from a YAML file."""
         need_update = False
         if charlist == "":
-            if self.char_list != self.server.char_list:
+            if self.char_list_ref != "":
                 self.char_list = self.server.char_list
                 need_update = True
         else:
-            new_chars = None
-            with open(f"storage/charlists/{charlist}.yaml", "r", encoding="utf-8") as chars:
-                new_chars = yaml.safe_load(chars)
-
-            if self.char_list != new_chars:
+            if self.char_list_ref != charlist:
+                new_chars = None
+                with open(f"storage/charlists/{charlist}.yaml", "r", encoding="utf-8") as chars:
+                    new_chars = yaml.safe_load(chars)
                 self.char_list = new_chars
                 need_update = True
         if need_update:
+            self.char_list_ref = charlist
             # self.char_emotes = {char: Emotes(char) for char in self.char_list}
             for client in self.clients:
                 self.send_characters(client)
