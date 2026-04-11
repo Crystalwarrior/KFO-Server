3083e48004a720c142d5eabff32324aa20fb4f34
Alex Noir <varsash@gmail.com>
2026-04-11 06:47:38 +0300

    oops

--- server/area_manager.py
+++ server/area_manager.py
@@ -2,7 +2,7 @@ from server import commands
 from server.exceptions import ClientError, AreaError, ArgumentError, ServerError
 from server.area import Area
 from collections import OrderedDict
-from constants import derelative
+from server.constants import derelative
 
 import oyaml as yaml  # ordered yaml
 import os
